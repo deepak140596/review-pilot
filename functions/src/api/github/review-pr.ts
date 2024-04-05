73c941ea84ecb92cdcb1c90cd416621a517c810b
Deepak Prasad <deepak140596@gmail.com>
2024-04-05 21:02:56 +0530

    configured octokit

---
+++ functions/src/api/github/review-pr.ts
@@ -0,0 +1,52 @@
+import { Octokit } from "octokit";
+import * as admin from "firebase-admin";
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import { createAppAuth } from "@octokit/auth-app";
+
+try {
+    admin.initializeApp();
+} catch{}
+const db = admin.firestore();
+const app = express();
+
+
+app.get('/', async (req, res) => {
+    const installationId = 49230850;
+    const githubData = (await db.doc('admin/github').get()).data() ?? {};
+    const privateKey = fixPrivateKeyFormat(githubData.private_key as string);
+    const appId = githubData.app_id as number;
+    const octokit = new Octokit({
+        authStrategy: createAppAuth,
+        auth: {
+            installationId: installationId,
+            privateKey: privateKey,
+            appId: appId,
+        }
+    });
+    const {
+        data: { slug },
+      } = await octokit.rest.apps.getAuthenticated();
+
+    await octokit.rest.issues.create({
+        owner: "deepak140596",
+        repo: "review-pilot",
+        title: "Hello world from " + slug,
+      });
+
+    res.status(200).send('PR review called');
+});
+
+export const reviewPr = functions.https.onRequest(app);
+
+function fixPrivateKeyFormat(privateKeyData: string) {
+
+	const keyWithouBeginStatement = privateKeyData.replace("-----BEGIN RSA PRIVATE KEY----- ", "");
+	const keyWithouEndStatement = keyWithouBeginStatement.replace(" -----END RSA PRIVATE KEY-----", "");
+  	const lines = keyWithouEndStatement.split(' ');
+  	const formattedPrivateKey = lines.join('\n');
+
+  	const fixedPrivateKey = `-----BEGIN RSA PRIVATE KEY-----\n${formattedPrivateKey}\n-----END RSA PRIVATE KEY-----`;
+
+  	return fixedPrivateKey;
+}
--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,6 +1,8 @@
 // /functions/src/api/index.ts
 import { webhook } from './github/webhook';
 import { callback } from './github/callback';
+import { reviewPr } from './github/review-pr';
 
 export const githubWebhook = webhook;
 export const githubCallback = callback;
+export const githubReviewPR = reviewPr;
